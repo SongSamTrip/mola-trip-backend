bd5750e10ea9b84b30238b75a03912f9ae9e9c0d
kwondh5217 <trewq231@naver.com>
2024-05-05 23:19:49 +0900

    Feat : ChatMessage Entity 추가

--- src/main/java/com/mola/domain/chat/ChatMessage.java
+++ src/main/java/com/mola/domain/chat/ChatMessage.java
@@ -4,15 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Setter
 @Getter
 @Entity
