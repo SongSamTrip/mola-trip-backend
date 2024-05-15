0acec184a837b47454661149b3aa705960b3a747
kwondh5217 <trewq231@naver.com>
2024-05-16 02:14:50 +0900

    Feat : TripImageRepository 연관관계 제거 기능 구현

--- src/main/java/com/mola/domain/tripBoard/repository/TripImageRepository.java
+++ src/main/java/com/mola/domain/tripBoard/repository/TripImageRepository.java
@@ -2,6 +2,14 @@ package com.mola.domain.tripBoard.repository;
 
 import com.mola.domain.tripBoard.entity.TripImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TripImageRepository extends JpaRepository<TripImage, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE TripImage ti SET ti.tripPost = null WHERE ti.tripPost.id = :tripPostId")
+    void detachOldImages(Long tripPostId);
 }
