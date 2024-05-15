efca4af8a068b1a89b59322bcaeff6e88396ca67
kwondh5217 <trewq231@naver.com>
2024-05-16 02:15:55 +0900

    Feat : TripPostController 게시글 저장 기능 구현

--- src/main/java/com/mola/domain/tripBoard/controller/TripPostController.java
+++ src/main/java/com/mola/domain/tripBoard/controller/TripPostController.java
@@ -1,10 +1,17 @@
 package com.mola.domain.tripBoard.controller;
 
+import com.mola.domain.tripBoard.dto.TripPostDto;
+import com.mola.domain.tripBoard.entity.TripPost;
 import com.mola.domain.tripBoard.service.TripPostService;
+import com.mola.global.exception.CustomException;
+import com.mola.global.exception.GlobalErrorCode;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +25,18 @@ public class TripPostController {
     @PostMapping("/draft")
     public ResponseEntity<Long> createDraftTripPost(){
         return ResponseEntity.status(HttpStatus.CREATED).body(tripPostService.createDraftTripPost());
+    }
+
+    @PostMapping
+    public ResponseEntity<TripPost> saveTripPost(@Valid @RequestBody TripPostDto tripPostDto,
+                                                 Errors errors){
+        if(errors.hasErrors()){
+            throw new CustomException(GlobalErrorCode.MissingRequireData);
+        }
+
+        TripPost save = tripPostService.save(tripPostDto);
+
+        return ResponseEntity.ok(save);
     }
 
 
