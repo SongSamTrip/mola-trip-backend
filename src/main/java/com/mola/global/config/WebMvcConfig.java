f770b54ecbba6e9a4ac34a55a5e9b6ffe952c598
kwondh5217 <trewq231@naver.com>
2024-05-16 02:17:26 +0900

    Feat : ModelMapper 빈 추가

--- src/main/java/com/mola/global/config/WebMvcConfig.java
+++ src/main/java/com/mola/global/config/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package com.mola.global.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +13,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*");
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
