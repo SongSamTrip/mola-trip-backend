63be00100e77a727cec46f61038be51b6882c781
kwondh5217 <trewq231@naver.com>
2024-05-16 02:17:08 +0900

    Feat : TripPostService 게시글 저장 기능 구현

--- src/main/java/com/mola/domain/tripBoard/service/TripPostService.java
+++ src/main/java/com/mola/domain/tripBoard/service/TripPostService.java
@@ -1,10 +1,12 @@
 package com.mola.domain.tripBoard.service;
 
+import com.mola.domain.tripBoard.dto.TripPostDto;
 import com.mola.domain.tripBoard.entity.TripPost;
 import com.mola.domain.tripBoard.repository.TripPostRepository;
 import com.mola.global.exception.CustomException;
 import com.mola.global.exception.GlobalErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class TripPostService {
 
     private final TripPostRepository tripPostRepository;
+    private final ModelMapper modelMapper;
 
     public boolean existsTripPost(Long id){
         return tripPostRepository.existsById(id);
@@ -24,5 +27,12 @@ public class TripPostService {
 
     public Long createDraftTripPost(){
         return tripPostRepository.save(TripPost.createDraft()).getId();
+    }
+
+    public TripPost save(TripPostDto tripPostDto){
+        TripPost tripPost = modelMapper.map(tripPostDto, TripPost.class);
+        tripPost.toPublic();
+
+        return tripPostRepository.save(tripPost);
     }
 }
