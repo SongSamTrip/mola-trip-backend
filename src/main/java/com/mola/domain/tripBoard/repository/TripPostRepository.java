7a74e3718e0b396350d3f689e63fda53cb07dccd
kwondh5217 <trewq231@naver.com>
2024-05-16 05:43:11 +0900

    Feat : 게시글 저장 기능 구현

--- src/main/java/com/mola/domain/tripBoard/repository/TripPostRepository.java
+++ src/main/java/com/mola/domain/tripBoard/repository/TripPostRepository.java
@@ -4,4 +4,6 @@ import com.mola.domain.tripBoard.entity.TripPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripPostRepository extends JpaRepository<TripPost, Long> {
+
+    TripPost save(TripPost tripPost);
 }
