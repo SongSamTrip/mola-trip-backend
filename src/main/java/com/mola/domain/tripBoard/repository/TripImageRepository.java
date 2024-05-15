73927d6d060c38c45afaa1632ab36eee266a64e0
kwondh5217 <trewq231@naver.com>
2024-05-16 00:13:46 +0900

    Feat : TripImageRepository 구현

---
+++ src/main/java/com/mola/domain/tripBoard/repository/TripImageRepository.java
@@ -0,0 +1,7 @@
+package com.mola.domain.tripBoard.repository;
+
+import com.mola.domain.tripBoard.entity.TripImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripImageRepository extends JpaRepository<TripImage, Long> {
+}
