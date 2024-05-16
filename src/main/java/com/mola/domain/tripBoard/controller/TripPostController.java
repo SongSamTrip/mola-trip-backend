c27ed28a0550c77c3bd06a10b24dcafa7fd6fda4
kwondh5217 <trewq231@naver.com>
2024-05-16 14:06:33 +0900

    Feat : TripPostController 조회, 삭제 기능 구현

--- src/main/java/com/mola/domain/tripBoard/controller/TripPostController.java
+++ src/main/java/com/mola/domain/tripBoard/controller/TripPostController.java
@@ -1,6 +1,7 @@
 package com.mola.domain.tripBoard.controller;
 
 import com.mola.domain.tripBoard.dto.TripPostDto;
+import com.mola.domain.tripBoard.dto.TripPostListResponseDto;
 import com.mola.domain.tripBoard.dto.TripPostResponseDto;
 import com.mola.domain.tripBoard.dto.TripPostUpdateDto;
 import com.mola.domain.tripBoard.entity.TripPost;
@@ -9,10 +10,13 @@ import com.mola.global.exception.CustomException;
 import com.mola.global.exception.GlobalErrorCode;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,6 +24,22 @@ import org.springframework.web.bind.annotation.*;
 public class TripPostController {
 
     private final TripPostService tripPostService;
+
+
+    @GetMapping
+    public ResponseEntity<List<TripPostListResponseDto>> getTripPosts(Pageable pageable) {
+        List<TripPostListResponseDto> allTripPosts =
+                tripPostService.getAllTripPosts(pageable);
+
+        return ResponseEntity.ok(allTripPosts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TripPostResponseDto> getTripPost(@PathVariable Long id) {
+        TripPost byId = tripPostService.findById(id);
+
+        return ResponseEntity.ok(TripPost.toTripPostResponseDto(byId));
+    }
 
     @PostMapping("/draft")
     public ResponseEntity<Long> createDraftTripPost(){
@@ -48,6 +68,13 @@ public class TripPostController {
         TripPostResponseDto responseDto = tripPostService.update(tripPostUpdateDto);
 
         return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTripPost(@PathVariable Long id){
+        tripPostService.deleteTripPost(id);
+
+        return ResponseEntity.ok("게시글이 삭제되었습니다.");
     }
 
 
