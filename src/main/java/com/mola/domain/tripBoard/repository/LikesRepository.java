7708314daa0476d11fa74abb8d416580032a4ea1
kwondh5217 <trewq231@naver.com>
2024-05-17 08:28:26 +0900

    Feat : LikesRepository 좋아요 중복체크 기능 구현

--- src/main/java/com/mola/domain/tripBoard/repository/LikesRepository.java
+++ src/main/java/com/mola/domain/tripBoard/repository/LikesRepository.java
@@ -2,7 +2,9 @@ package com.mola.domain.tripBoard.repository;
 
 import com.mola.domain.tripBoard.entity.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-    
+    @Query("SELECT COUNT(l) > 0 FROM Likes l WHERE l.member.id = :memberId AND l.tripPost.id = :tripPostId")
+    boolean existsByMemberIdAndTripPostId(Long memberId, Long tripPostId);
 }
