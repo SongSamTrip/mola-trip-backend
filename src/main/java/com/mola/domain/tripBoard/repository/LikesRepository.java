f1e242d33bc052ed45d4e3e780333c5340fa7d6c
kwondh5217 <trewq231@naver.com>
2024-05-17 11:10:31 +0900

    Feat : LikesRepository 조회 메서드 구현

--- src/main/java/com/mola/domain/tripBoard/repository/LikesRepository.java
+++ src/main/java/com/mola/domain/tripBoard/repository/LikesRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     @Query("SELECT COUNT(l) > 0 FROM Likes l WHERE l.member.id = :memberId AND l.tripPost.id = :tripPostId")
     boolean existsByMemberIdAndTripPostId(Long memberId, Long tripPostId);
+
+    @Query("SELECT l FROM Likes l WHERE l.member.id = :memberId AND l.tripPost.id = :tripPostId")
+    Likes findByMemberIdAndTripPostId(Long memberId, Long tripPostId);
 }
