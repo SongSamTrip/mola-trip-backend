88600a43da6b0a3297285da318e1951b74572c48
kwondh5217 <trewq231@naver.com>
2024-05-22 04:40:51 +0900

    Refactor : QueryDsl 적용 방식 변경

--- src/main/java/com/mola/domain/tripBoard/tripPost/repository/TripPostRepositoryCustom.java
+++ src/main/java/com/mola/domain/tripBoard/tripPost/repository/TripPostRepositoryCustom.java
@@ -1,16 +1,19 @@
 package com.mola.domain.tripBoard.tripPost.repository;
 
+import com.mola.domain.tripBoard.comment.dto.CommentDto;
 import com.mola.domain.tripBoard.tripPost.dto.TripPostListResponseDto;
 import com.mola.domain.tripBoard.tripPost.dto.TripPostResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface TripPostQueryRepository {
+public interface TripPostRepositoryCustom {
 
     TripPostResponseDto getTripPostResponseDtoById(Long tripPostId, Long memberId);
 
     Page<TripPostListResponseDto> getAllTripPostResponseDto(Pageable pageable);
 
     boolean isPublic(Long id);
+
+    Page<CommentDto> getCommentsForTripPost(Long tripPostId, Pageable pageable);
 
 }
